8d945239176329589a2dac6bba93963f0922bbcc
Thocuteee <thoch3765@ut.edu.vn>
2026-03-26 14:36:49 +0700

    da fix

--- backend/auth-service/auth/src/main/java/uth/edu/auth/dto/LoginRequest.java
+++ backend/auth-service/auth/src/main/java/uth/edu/auth/dto/LoginRequest.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @Getter
