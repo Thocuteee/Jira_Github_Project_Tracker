0b7d1a62bd6ef36b572949b532f0f34820fe0d58
HPhi1808 <hoangphi120705@gmail.com>
2026-04-12 09:29:15 +0700

    KAN-26: Hoàn thiện phần xem chi tiết Task

---
+++ frontend/src/api/requirement.service.ts
@@ -0,0 +1,19 @@
+import axiosClient from './authClient';
+
+const BASE_PATH = '/api/requirements';
+
+export interface Requirement {
+  id: string;
+  groupId: string;
+  title: string;
+  description: string;
+  createdAt?: string;
+}
+
+class RequirementService {
+  async getRequirementsByGroup(groupId: string): Promise<Requirement[]> {
+    return (await axiosClient.get(`${BASE_PATH}/group/${groupId}`)) as Requirement[];
+  }
+}
+
+export default new RequirementService();
